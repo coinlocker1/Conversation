9beabbdcaa57644e4b6aba173af39b284eb31918
1503011 <1503011>
2016-07-25 07:46:14 +0900

    JavaScript修正

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -11,12 +11,12 @@ var UserComment2 = new Array(3);
 var UserComment3 = new Array(3);
 
 /*AIのコメント*/
-var AIComment1 = [ "ありがとうございます" , "こんにちは" , "こんばんは" ];
+var AIComment1 = [ "ありがとうございます" , "" , "" ];
 var AIComment2 = [ "もちろんです","","" ];
-var AIComment3 = [ "さようなら","test1","test1" ];
+var AIComment3 = [ "さようなら","","" ];
 /*選択肢*/
-var UserComment1 = [ "明日もよろしくね","test1","test1" ];
-var UserComment2 = [ "バイバイ","test2","test2" ];
+var UserComment1 = [ "明日もよろしくね","","" ];
+var UserComment2 = [ "バイバイ","","" ];
 
 
 /*読み込み完了時の実行内容*/
@@ -42,8 +42,6 @@ window.onload = function Greeting() {
 	flg = 0;
 }
 
-/*button1.id = UserComment1[0];
-
 /*ボタン1が押された場合の処理*/
 function bt1push() {
 	switch (flg){
@@ -68,12 +66,17 @@ function bt2push() {
 	switch (flg){
   case 0:
     tx.value = (AIComment1[1]);
+    button2.value = (UserComment2[1]);
+    flg ++;
     break;
   case 1:
     tx.value = (AIComment2[1]);
+    button2.value = (UserComment2[1]);
+    flg ++;
     break;
   case 2:
     tx.value = (AIComment3[1]);
+    flg ++;
     break;
     }
 }
@@ -83,12 +86,17 @@ function bt3push() {
 	switch (flg){
   case 0:
     tx.value = (AIComment1[2]);
+    button3.value = (UserComment2[2]);
+    flg ++;
     break;
   case 1:
     tx.value = (AIComment2[2]);
+    button3.value = (UserComment2[2]);
+    flg ++;
     break;
   case 2:
     tx.value = (AIComment3[2]);
+    flg ++;
     break;
     }
 }
